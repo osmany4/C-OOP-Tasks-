0ff133eef62fa30f5f8d0c708036ad1b1cae03fa
osmany4 <101186496+osmany4@users.noreply.github.com>
2024-01-29 12:46:48 +0000

    Add files via upload

---
+++ Week 3 activity encapsulation and data abstraction.cs
@@ -0,0 +1,31 @@
+namespace Mygame
+
+public Player 
+ {
+    private int getHealth()
+    { return _health;}
+    public void setHealth (int thisHealth)
+    {_health = thisHealt;}
+    
+
+ }
+
+
+
+ abstract class InventoryItem
+ {
+    protected string _name;
+    {
+        set _name;
+    }
+
+    public string GetName()
+    {
+        return GetName();
+    }
+    
+    public abstract void Use();
+
+
+ }
+
