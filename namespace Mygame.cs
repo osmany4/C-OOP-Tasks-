b33b26a381e3556de11810e2d5f5d9a74cc3cf9c
osmany4 <101186496+osmany4@users.noreply.github.com>
2024-02-05 12:28:51 +0000

    Add files via upload

---
+++ namespace Mygame.cs
@@ -0,0 +1,46 @@
+namespace Mygame
+
+public class Character
+
+{
+    public string name;
+    public  int health;
+    public  int attackPower;
+
+    public void Attack(Character target)
+    {
+        target.health = health - attackPower;
+    }
+
+
+    public void TakeDamage(target.health)
+    {
+
+    }
+    public class Player:Character
+
+    public class Enemey:Character
+}
+
+
+
+public class GameObject 
+{
+    public string name;
+    public double PositionX;
+    public double  PositionY;
+
+    public void Interact()
+    {
+        Console.Writeline("Player collects item");
+    }
+
+    public class PowerUp:GameObject
+        
+      public string Effect;
+        }
+            public override void Interact() 
+            {
+                Console.Writeline("Player health boost expires in 10 seconds")
+            }        
+}
